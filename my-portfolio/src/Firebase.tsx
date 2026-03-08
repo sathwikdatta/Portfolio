1ef12af567e0ab52d65757c183240cd2ae9068c2
Sathwik Datta <dsathwikdatta@gmail.com>
2026-03-08 19:21:21 +0530

    Revert "Move Firebase config to env vars and ignore .env"
    This reverts commit 1944e0bc71ad23a4ae6acbd86f3e8298a2db7c20.

--- my-portfolio/src/Firebase.tsx
+++ my-portfolio/src/Firebase.tsx
@@ -1,21 +1,15 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "",
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "",
+  apiKey: "AIzaSyD_RmbozwMrQCjHDC6V_2A4hL5IlPFodCk",
+  authDomain: "portfolio-ae62d.firebaseapp.com",
+  projectId: "portfolio-ae62d",
+  storageBucket: "portfolio-ae62d.firebasestorage.app",
+  messagingSenderId: "676890360783",
+  appId: "1:676890360783:web:e82aecc8d0f5c99066b0ba",
+  measurementId: "G-8T2NG0X8EN"
 };
-
-if (!firebaseConfig.apiKey) {
-  // Keep a clear error in local/dev when env vars are missing.
-  console.error("Firebase config is missing. Set REACT_APP_FIREBASE_* variables.");
-}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
